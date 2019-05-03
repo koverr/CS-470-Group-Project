90195a5cfed32d98ce314938ff4b7b7284c1e4d4
Tars <taa522@Mail.umkc.edu>
2019-05-03 16:55:01 -0500

    Added functionality for User Page choice boxes

--- src/CoolCars/UserPageController.java
+++ src/CoolCars/UserPageController.java
@@ -5,10 +5,91 @@
  */
 package CoolCars;
 
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
 /**
+ * FXML Controller class
  *
- * @author koryo
+ * @author mytar
  */
-public class UserPageController {
+public class UserPageController implements Initializable {
+    SQLConnection sqlconn = new SQLConnection();
+    Connection conn = sqlconn.connect();
+    Statement stmt = sqlconn.getStatement();
+    
+    @FXML
+    ChoiceBox Location;
+    
+    @FXML 
+    ChoiceBox Year;
+    
+    @FXML
+    TextField Price;
+    
+    @FXML
+    TextField Results;
+    
+    @FXML
+    private void handleSelection() throws IOException{
+  
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+            ResultSet rs = stmt.executeQuery("SELECT Address FROM Location;");
+            System.out.println("test");
+            while(rs.next()){
+                Location.getItems().add(rs.getString(1));
+            }
+            
+            rs = stmt.executeQuery("SELECT DISTINCT Year FROM Cars;");
+            while(rs.next()){
+                Year.getItems().add(rs.getString(1));
+            }
+            
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }    
     
 }
