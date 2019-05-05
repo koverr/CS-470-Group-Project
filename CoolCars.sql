29c34db8a339c31aaae9b980feafeedc20f408e1
Alec Shern <alecshern@gmail.com>
2019-05-05 16:10:23 -0500

    Added aes_encryption to passwords in SQL script

--- CoolCars.sql
+++ CoolCars.sql
@@ -17,13 +17,13 @@ end //
 CREATE PROCEDURE user_login(IN u VARCHAR(30),
                             IN p VARCHAR(20))
 BEGIN
-    SELECT Username FROM User WHERE Username = u AND Password = p;
+    SELECT Username FROM User WHERE Username = u AND Password = aes_encrypt(p, u);
 end //
 
 CREATE PROCEDURE employee_login(IN eid INT,
                                 IN p VARCHAR(20))
 BEGIN
-    SELECT EmployeeID FROM Employee WHERE EmployeeID = eid AND Password = p;
+    SELECT EmployeeID FROM Employee WHERE EmployeeID = eid AND Password = aes_encrypt(p, eid);
 END //
 DELIMITER ;
 
@@ -39,7 +39,7 @@ CREATE TABLE Location (
 
 CREATE TABLE Employee (
 	EmployeeID INT NOT NULL,
-    Password VARCHAR (20) NOT NULL,
+    Password VARCHAR (50) NOT NULL,
     Name VARCHAR (25) NOT NULL,
     EmployeeType INT NOT NULL,
     StoreID INT NOT NULL,
@@ -62,7 +62,7 @@ CREATE TABLE Cars (
 
 CREATE TABLE User (
 	Username VARCHAR (30) NOT NULL,
-    Password VARCHAR (20) NOT NULL,
+    Password VARCHAR (50) NOT NULL,
     Name VARCHAR (25) NOT NULL,
     Age INT NOT NULL,
     CreditScore INT NOT NULL,
@@ -105,29 +105,29 @@ VALUES
 	(9374, 1, 1, 'Sport', 'Pagani', 'Zonda HP Barchetta', 2017, 'Silver', 530);
     
 INSERT INTO User (Username, Password, Name, Age, CreditScore, License, Insurance, Balance, PhoneNumber)
-VALUES ('koverbay', 'pass', 'Kory Overbay', 20, 400, 3333, 3434, 0, '(816)673-8479'),
-	('user1', 'pass1', 'Bob Builder', 43, 400, 1234, 5678, 0, '(816)712-3245'),
-	('user2', 'pass2', 'Dora Explora', 18, 200, 3245, 8573, 3000, '(281)343-2367'),
-    ('user3', 'pass3', 'Jimmy Neutron', 32, 350, 2024, 3951, 0, '(382)333-5973'),
-    ('user4', 'pass4', 'Fred Rogers', 51, 530, 3942, 6941, 0, '(394)359-6998'),
-    ('user5', 'pass5', 'Steve Rogers', 30, 450, 5908, 0899, 2500, '(959)844-6327'),
-    ('user6', 'pass6', 'Charlie Brown', 21, 390, 9943, 7462, 6000, '(733)573-8788'),
-    ('user7', 'pass7', 'Jack Daniels', 57, 500, 8612, 8374, 0, '(816)838-3342'),
-    ('user8', 'pass8', 'Lucile Ball', 38, 375, 8382, 7633, 1750, '(211)382-3733'),
-    ('user9', 'pass9', 'Tommy Tutone', 45, 642, 6634, 1332, 1000, '(722)867-5309'),
-    ('user10', 'pass10', 'Alec Baldwin', 56, 300, 5643, 8843, 0, '(833)356-4656');
+VALUES ('koverbay', aes_encrypt('pass', 'koverbay'), 'Kory Overbay', 20, 400, 3333, 3434, 0, '(816)673-8479'),
+	('user1', aes_encrypt('pass1', 'user1'), 'Bob Builder', 43, 400, 1234, 5678, 0, '(816)712-3245'),
+	('user2', aes_encrypt('pass2', 'user2'), 'Dora Explora', 18, 200, 3245, 8573, 3000, '(281)343-2367'),
+    ('user3', aes_encrypt('pass3', 'user3'), 'Jimmy Neutron', 32, 350, 2024, 3951, 0, '(382)333-5973'),
+    ('user4', aes_encrypt('pass4', 'user4'), 'Fred Rogers', 51, 530, 3942, 6941, 0, '(394)359-6998'),
+    ('user5', aes_encrypt('pass5', 'user5'), 'Steve Rogers', 30, 450, 5908, 0899, 2500, '(959)844-6327'),
+    ('user6', aes_encrypt('pass6', 'user6'), 'Charlie Brown', 21, 390, 9943, 7462, 6000, '(733)573-8788'),
+    ('user7', aes_encrypt('pass7', 'user7'), 'Jack Daniels', 57, 500, 8612, 8374, 0, '(816)838-3342'),
+    ('user8', aes_encrypt('pass8', 'user8'), 'Lucile Ball', 38, 375, 8382, 7633, 1750, '(211)382-3733'),
+    ('user9', aes_encrypt('pass9', 'user9'), 'Tommy Tutone', 45, 642, 6634, 1332, 1000, '(722)867-5309'),
+    ('user10', aes_encrypt('pass10', 'user10'), 'Alec Baldwin', 56, 300, 5643, 8843, 0, '(833)356-4656');
 
 INSERT INTO Employee (EmployeeID, Password, Name, EmployeeType, StoreID, PhoneNumber)
-VALUES (1, 'password1', 'Robert Smith', 1, 1, '(281)342-2367'),
-  	(2, 'password2', 'Mark Markus', 2, 1,'(816)245-1356'),
-	(3, 'password3', 'Walter Twoshoes', 1, 2,'(913)183-1945'),
-    (4, 'password4', 'Samus Metroid', 2, 2,'(913)917-1845'),
-    (5, 'password5', 'Steven Even', 1, 3,'(913)819-1924'),
-    (6, 'password6', 'Steeeeeven Eeeeeven', 2, 3,'(816)817-1845'),
-    (7, 'password7', 'Mario Peach', 1, 2,'(816)294-1935'),
-    (8, 'password8', 'Sasquatch Hair', 2, 2,'(913)817-8495'),
-    (9, 'password9', 'Philipo Demagio', 1, 1,'(816)917-8562'),
-    (10, 'password10', 'Jacque Frenchie', 2, 3,'(913)846-1947');
+VALUES (1, aes_encrypt('password1', '1'), 'Robert Smith', 1, 1, '(281)342-2367'),
+  	(2, aes_encrypt('password2', '2'), 'Mark Markus', 2, 1,'(816)245-1356'),
+	(3, aes_encrypt('password3', '3'), 'Walter Twoshoes', 1, 2,'(913)183-1945'),
+    (4, aes_encrypt('password4', '4'), 'Samus Metroid', 2, 2,'(913)917-1845'),
+    (5, aes_encrypt('password5', '5'), 'Steven Even', 1, 3,'(913)819-1924'),
+    (6, aes_encrypt('password6', '6'), 'Steeeeeven Eeeeeven', 2, 3,'(816)817-1845'),
+    (7, aes_encrypt('password7', '7'), 'Mario Peach', 1, 2,'(816)294-1935'),
+    (8, aes_encrypt('password8', '8'), 'Sasquatch Hair', 2, 2,'(913)817-8495'),
+    (9, aes_encrypt('password9', '9'), 'Philipo Demagio', 1, 1,'(816)917-8562'),
+    (10, aes_encrypt('password10', '10'), 'Jacque Frenchie', 2, 3,'(913)846-1947');
 
 /*INSERT INTO Rents (VIN, Username, Duration)
 VALUES (
