cd2b882572d31f61134b3a14e0b1efbc1ecfc9e9
Alec Shern <alecshern@gmail.com>
2019-05-05 23:48:34 -0500

    More Comments

--- src/CoolCars/SQLConnection.java
+++ src/CoolCars/SQLConnection.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package CoolCars;
 import java.sql.*;
 import java.util.ResourceBundle;
@@ -13,10 +8,12 @@ public class SQLConnection {
     private ResourceBundle reader = null;
     private static String FILENAME;
     
+    //Points to a config file outside of code
     public SQLConnection(){
         FILENAME = "resources/dbconfig";
     }
 
+    //Connects to the database without having the plaintext user/pass/connection string in the code
     public Connection connect(){
         try {
             reader = ResourceBundle.getBundle(FILENAME);
